3f8b7fab318f915922da2ba16389e09f1746ff8c
Luiz Mai <lffmai@gmail.com>
2019-12-07 21:51:51 -0300

    Including config file

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Global GlobalConfig
+	HTTP   HTTPConfig
+}
+
+type GlobalConfig struct {
+	Name string
+}
+
+type HTTPConfig struct {
+	Port int
+}
+
+func Read(configPaths ...string) (cfg Config, err error) {
+	v := viper.New()
+	v.SetConfigName("config")
+	v.SetConfigType("toml")
+	v.AutomaticEnv()
+	v.AddConfigPath("./")
+	v.AddConfigPath("./../")
+	if err = v.ReadInConfig(); err != nil {
+		return cfg, fmt.Errorf("Failed to read the configuration file: %s", err)
+	}
+	if err = v.Unmarshal(&cfg); err != nil {
+		return cfg, err
+	}
+	return cfg, nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+
+
+	"github.com/luiz-mai/go-api-boilerplate/config"
+)
+
 func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	return
 }
